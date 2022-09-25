24764607d7a800f8830b9f9ab7c993cdff6af816
Clark Kenneth Racadio <kennethracadio@hotmail.com>
2022-09-25 19:09:00 -0400

    style Header component's desktop view

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,47 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import styles from './Header.module.scss'
+import cn from 'classnames'
+
+const Links = [
+	{ path: '/', text: 'HOME' },
+	{ path: '/schedule', text: 'SCHEDULE' },
+	{ path: '/wedding-party', text: 'WEDDING PARTY' },
+	{ path: '/photos', text: 'PHOTOS' },
+	{ path: '/things-to-do', text: 'THINGS TO DO' },
+	{ path: '/faqs', text: 'FAQS' },
+]
 
 const Header = () => {
+	const { asPath } = useRouter()
+
 	return (
-		<header>
-			<div>
+		<header className={styles.header}>
+			<div className={styles['header__wedding-info']}>
 				<h5>August 5, 2023</h5>
 				<h5>Toronto, ON</h5>
 			</div>
+
 			<Link href='/'>
-				<h2>
+				<h2 className={styles.header__title}>
 					<a>Kenneth & Rebecca</a>
 				</h2>
 			</Link>
 			<nav>
-				<ul>
-					<li>
-						<Link href='/'>
-							<a>Home</a>
-						</Link>
-					</li>
-					<li>
-						<Link href='/schedule'>
-							<a>SCHEDULE</a>
-						</Link>
-					</li>
-					<li>
-						<Link href='/wedding-party'>
-							<a>WEDDING PARTY</a>
-						</Link>
-					</li>
-					<li>
-						<Link href='/photos'>
-							<a>PHOTOS</a>
-						</Link>
-					</li>
-					<li>
-						<Link href='/things-to-do'>
-							<a>THINGS TO DO</a>
-						</Link>
-					</li>
-					<li>
-						<Link href='/faqs'>
-							<a>FAQS</a>
-						</Link>
-					</li>
+				<ul className={styles.header__nav}>
+					{Links.map(({ path, text }) => (
+						<li
+							key={path}
+							className={cn(styles['header__nav-item'], {
+								[styles['header__nav-item--active']]: asPath === path,
+							})}
+						>
+							<Link href={path}>
+								<a>{text}</a>
+							</Link>
+						</li>
+					))}
 				</ul>
 			</nav>
 		</header>
