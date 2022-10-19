5475ccaa7418cb27065869b83c2033988bb5bde1
Clark Kenneth Racadio <kennethracadio@hotmail.com>
2022-10-18 22:13:03 -0400

    wedding - remove console.log

--- apps/wedding/src/pages/wedding-party.tsx
+++ apps/wedding/src/pages/wedding-party.tsx
@@ -6,45 +6,41 @@ import { fetchAPI, getStrapiMedia } from '@src/lib/api'
 
 const WeddingParty = ({
 	friends,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
-	console.log('friends', friends)
-
-	return (
-		<div className='page-container'>
-			<div className='page-header'>
-				<h1 className='page-title'>Wedding Party</h1>
-				<p className='page-description'>
-					Meet our family and friends who are walking down the aisle with us.
-				</p>
-			</div>
-			<ul className={cn('card-list', styles['responsive-card-list'])}>
-				{friends.map(
-					({ id, attributes: { name, role, description, image } }, i) => (
-						<React.Fragment key={id}>
-							<li className='card'>
-								<img
-									className={styles['img']}
-									src={getStrapiMedia(image)}
-									alt={`${name}`}
-								/>
-								<h3 className='card-title'>{name}</h3>
-								<h4 className='card-subtitle'>{role}</h4>
-								<p className='card-description'>{description}</p>
-							</li>
-							{i % 2 === 0 && (
-								<li
-									className='separator'
-									aria-hidden='true'
-									role='presentation'
-								/>
-							)}
-						</React.Fragment>
-					)
-				)}
-			</ul>
+}: InferGetStaticPropsType<typeof getStaticProps>) => (
+	<div className='page-container'>
+		<div className='page-header'>
+			<h1 className='page-title'>Wedding Party</h1>
+			<p className='page-description'>
+				Meet our family and friends who are walking down the aisle with us.
+			</p>
 		</div>
-	)
-}
+		<ul className={cn('card-list', styles['responsive-card-list'])}>
+			{friends.map(
+				({ id, attributes: { name, role, description, image } }, i) => (
+					<React.Fragment key={id}>
+						<li className='card'>
+							<img
+								className={styles['img']}
+								src={getStrapiMedia(image)}
+								alt={`${name}`}
+							/>
+							<h3 className='card-title'>{name}</h3>
+							<h4 className='card-subtitle'>{role}</h4>
+							<p className='card-description'>{description}</p>
+						</li>
+						{i % 2 === 0 && (
+							<li
+								className='separator'
+								aria-hidden='true'
+								role='presentation'
+							/>
+						)}
+					</React.Fragment>
+				)
+			)}
+		</ul>
+	</div>
+)
 
 export async function getStaticProps() {
 	const friendsRes = await fetchAPI('/friends', { populate: '*' })
