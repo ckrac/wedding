80684c4f23e9a05949d4de9585c2a2c25bff6eb0
Clark Kenneth Racadio <kennethracadio@hotmail.com>
2022-10-19 22:29:58 -0400

    wedding - use cms to get photos data

--- apps/wedding/src/pages/photos.tsx
+++ apps/wedding/src/pages/photos.tsx
@@ -1,46 +1,8 @@
-import type { NextPage } from 'next'
+import type { InferGetStaticPropsType } from 'next'
 import styles from '@styles/pages/photo.module.scss'
+import { fetchAPI, getStrapiMedia } from '@src/lib/api'
 
-const Images = [
-	{
-		src: 'https://images.zola.com/0f14b8a8-ea17-40a6-a0cd-5dde20921062?w=800&h=800',
-		alt: '',
-	},
-	{
-		src: 'https://images.zola.com/ab57f490-5561-4f8b-8fbb-9beb39cadb46?w=800&h=800',
-		alt: '',
-	},
-	{
-		src: 'https://images.zola.com/1787293e-0f3e-469c-be55-c52dd13b5ed4?w=800&h=800',
-		alt: '',
-	},
-	{
-		src: 'https://images.zola.com/c9745809-0c7d-4143-a064-a3a9610a74d9?w=800&h=800',
-		alt: '',
-	},
-	{
-		src: 'https://images.zola.com/e8e663f9-e94b-4cf7-8432-b23cb5d124a4?w=800&h=800',
-		alt: '',
-	},
-	{
-		src: 'https://images.zola.com/4d47a172-8f95-46ce-addb-16b448c8cca6?w=800&h=800',
-		alt: '',
-	},
-	{
-		src: 'https://images.zola.com/d17b5d05-ddff-4afc-aa6f-e7f5bedad42e?w=800&h=800',
-		alt: '',
-	},
-	{
-		src: 'https://images.zola.com/6f582e02-937a-4dee-a140-ed65af0d9c53?w=800&h=800',
-		alt: '',
-	},
-	{
-		src: 'https://images.zola.com/caa69165-a090-4299-816e-e7fdc7722b71?w=800&h=800',
-		alt: '',
-	},
-]
-
-const Photos: NextPage = () => {
+const Photos = ({ photos }: InferGetStaticPropsType<typeof getStaticProps>) => {
 	return (
 		<div className='page-container'>
 			<div className='page-header'>
@@ -50,12 +12,30 @@ const Photos: NextPage = () => {
 				</p>
 			</div>
 			<div className={styles['photos-container']}>
-				{Images.map(({ alt, src }) => (
-					<img key={src} className={styles.photo} src={src} alt={alt} />
+				{photos.map(({ id, attributes: { image } }) => (
+					<img key={id} className={styles.photo} src={getStrapiMedia(image)} />
 				))}
 			</div>
 		</div>
 	)
+}
+
+export async function getStaticProps() {
+	const res = await fetchAPI('/photos', { populate: '*' })
+
+	return {
+		props: {
+			photos: res.data as {
+				id: number
+				attributes: {
+					image: {
+						data: {}
+					}
+				}
+			}[],
+		},
+		revalidate: 1,
+	}
 }
 
 export default Photos
